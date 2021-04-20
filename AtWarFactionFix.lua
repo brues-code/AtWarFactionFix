f5d4569533e1d4ac45c3d47218bb8bff68650ba2
Skyler Hiscock <skylerhiscock@gmail.com>
2021-04-20 04:57:50 -0500

    Initial commit

---
+++ AtWarFactionFix.lua
@@ -0,0 +1,43 @@
+local addonName = ...
+local f = CreateFrame("FRAME")
+
+local PROTECTED_FACTIONS = { 
+    1119 -- Sons of Hodir
+}
+
+function f:OnEvent(event, arg1)
+    if event == "ADDON_LOADED" and arg1 == addonName then
+        self:OnLoad()
+    elseif event == "UNIT_FACTION" then
+        self:UNIT_FACTION(arg1)
+    end
+end
+
+function f:OnLoad()
+	self:RegisterEvent("UNIT_FACTION")
+    self:SetProtectedFactionNames()
+	self:UNIT_FACTION("player")
+end
+
+function f:SetProtectedFactionNames()
+    self.protectedFactionNames = {}
+    for k, v in pairs(PROTECTED_FACTIONS) do
+        local name = GetFactionInfoByID(v)
+        if name then
+            self.protectedFactionNames[name] = true
+        end
+    end
+end
+
+function f:UNIT_FACTION(unit)
+    if unit ~= "player" then return end
+    for factionIndex = 1, GetNumFactions() do
+        local name, _, _, _, _, _, atWarWith, canToggleAtWar = GetFactionInfo(factionIndex)
+        if atWarWith and canToggleAtWar and self.protectedFactionNames[name] then
+            FactionToggleAtWar(factionIndex)
+        end
+    end
+end
+
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", f.OnEvent)
